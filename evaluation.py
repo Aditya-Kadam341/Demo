226ca7a63aee8ff4eef0c4981a2b431cf0896b9e
Aditya-Kadam341 <166985454+Aditya-Kadam341@users.noreply.github.com>
2024-04-23 14:40:14 +0200

    F1 Score, Precision, Recall

--- evaluation.py
+++ evaluation.py
@@ -1,32 +1,15 @@
-true_tags = ["NN", "DT", "P", "HYP"]
-dummy_tags = ["NN", "VB", "P", "P"]
-# tp = 0
-# tn = 0
-# fn = 0
-# fp = 0
-tag = "P"
-# if (len(y_true)) == (len(y_pred)):
-#     for i in range(len(y_true)):
-#         print(y_true[i],y_pred[i])
-#         if (y_true[i] == label ) and (y_pred[i] == label):
-#             tp = tp+1
-#         elif (y_true[i] == label ) and  (y_pred[i] != label):
-#             fn = fn+1
-#         elif (y_true[i] != label ) and  (y_pred[i] == label):
-#             fp = fp+1
-#         elif (y_true[i] != label ) and  (y_pred[i] != label):
-#             tn=tn+1
-# print(tp,tn,fp,fn)
-# print("sum of all",tp+tn+fp+fn)
-# Precision = tp/(tp+fp)
-# Recall = tp/(tp+fn)
-# F1Score= 2*(Precision*Recall)/(Precision + Recall)
-# print("Precision of Tag", label, ":",Precision)
-# print("Recall of Tag", label, ":",Recall)
-# print("F1Score of Tag", label, ":",F1Score)
+########## Temporary Data ##########
 
+true_tags = ["NN", "DT", "P", "HYP"] #correct(GoldStandard data)
+dummy_tags = ["NN", "VB", "P", "P"]  #predicted(Dummy data)
+tag = "P" # Single label
+
+########## Temporary Data ##########
+
+########## Function to get tp,fp,fn,tn values ##########
 
 def matrix_values(y_pred,y_true,label):
+    # initialize variables 
     true_positive = 0
     true_negative = 0
     false_negative = 0
@@ -41,11 +24,32 @@ def matrix_values(y_pred,y_true,label):
                 false_positive = false_positive+1
             elif (y_true[i] != label ) and  (y_pred[i] != label):
                 true_negative=true_negative+1
-        # print("True positive",tp)
-        # print("False positive",fp)
-        # print("False negative",fn)
-        # print("True negative",tn)
+    else:
+        print("******\nTHE LENGTH OF CORRECT and PREDICTED LIST IS DIFFERENT\n*****")
+    # print("True positive1",true_positive)
+    # print("False positive1",false_positive)
+    # print("False negative1",false_negative)
+    # print("True negative1",true_negative)
     return [[true_positive, false_positive],[false_negative,true_negative]]
 
-print(matrix_values(true_tags,dummy_tags,tag))
+########## Function to get tp,fp,fn,tn values ##########
+
+# print(matrix_values(true_tags,dummy_tags,tag))
+
+def calculate_precision_recall(y_pred,y_true,label):
+    values = matrix_values(y_pred,y_true,label)
+    Precision = values[[0][0]][0]/(values[[0][0]][0]+values[[0][0]][1])
+    Recall = values[[0][0]][0]/(values[[0][0]][0]+values[[1][0]][0])
+    # print("True negative",values[[1][0]][1])
+    return Precision,Recall
+
+# print(calculate_precision_recall(true_tags,dummy_tags,tag))
+
+def calculate_f1score(y_pred,y_true,label):
+    Precision,Recall = calculate_precision_recall(y_pred,y_true,label)
+    F1_Score= 2*(Precision*Recall)/(Precision + Recall)
+    return F1_Score
+
+print(calculate_f1score(true_tags,dummy_tags,tag))
+
 
