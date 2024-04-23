3a177c081b5222c98d63a0c0eea81f888276d930
Aditya-Kadam341 <166985454+Aditya-Kadam341@users.noreply.github.com>
2024-04-23 15:22:54 +0200

    position of parameter changed

--- evaluation.py
+++ evaluation.py
@@ -7,7 +7,7 @@ tag = "P" # Single label
 
 
 ########## Function to get tp,fp,fn,tn values ##########
-def matrix_values(y_pred,y_true,label):
+def matrix_values(y_true,y_pred,label): # check position of parameter
     # initialize variables 
     true_positive = 0
     true_negative = 0
@@ -34,13 +34,13 @@ def matrix_values(y_pred,y_true,label):
 
 
 
-# print(matrix_values(true_tags,dummy_tags,tag)) #testing function matrix_values
+print(matrix_values(true_tags,dummy_tags,tag)) #testing function matrix_values
 
 
 
 ########## Function to calculate Precision & Recall ##########
-def calculate_precision_recall(y_pred,y_true,label):
-    values = matrix_values(y_pred,y_true,label)             # call matrix_values to get tp,fp,fn,tn values
+def calculate_precision_recall(y_true,y_pred,label):
+    values = matrix_values(y_true,y_pred,label)             # call matrix_values to get tp,fp,fn,tn values
     # Formula for Precision & Recall 
     Precision = values[[0][0]][0]/(values[[0][0]][0]+values[[0][0]][1])  #since i stored it in nested list/array this is how we get the values 
     Recall = values[[0][0]][0]/(values[[0][0]][0]+values[[1][0]][0])    
@@ -50,20 +50,20 @@ def calculate_precision_recall(y_pred,y_true,label):
 
 
 
-# print(calculate_precision_recall(true_tags,dummy_tags,tag)) #testing function calculate_precision_recall
+print(calculate_precision_recall(true_tags,dummy_tags,tag)) #testing function calculate_precision_recall
 
 
 
 ########## Function to calculate F1_Score ##########
-def calculate_f1score(y_pred,y_true,label):
-    Precision,Recall = calculate_precision_recall(y_pred,y_true,label) # call matrix_values to get tp,fp,fn,tn values
+def calculate_f1score(y_true,y_pred,label):
+    Precision,Recall = calculate_precision_recall(y_true,y_pred,label) # call matrix_values to get tp,fp,fn,tn values
     F1_Score= 2*(Precision*Recall)/(Precision + Recall)
     return F1_Score
 ########## Function to calculate F1_Score ##########
 
 
 
-# print(calculate_f1score(true_tags,dummy_tags,tag)) #testing function calculate_f1score
+print(calculate_f1score(true_tags,dummy_tags,tag)) #testing function calculate_f1score
 
 
 
